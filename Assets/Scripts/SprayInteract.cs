3df517a6c5eef025a623f2feb362be6ac8eb3610
MaxWillekes <max.willekes@student.hku.nl>
2019-05-16 20:52:47 +0200

    Initial commit

---
+++ Assets/Scripts/SprayInteract.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprayInteract : MonoBehaviour
+{
+    SphereCollider ownCollider;
+    GameObject hitObject;
+    // Update is called once per frame
+    private void Start()
+    {
+        ownCollider = gameObject.AddComponent<SphereCollider>();
+        //ownCollider.convex = true;
+        ownCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "TagAbleHuman")
+        {
+            Debug.Log(other.name);
+            other.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+}
