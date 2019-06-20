7623506f3870ec1ff1230a8710a1360cbb4bb04f
MaxWillekes <max.willekes@student.hku.nl>
2019-06-20 13:42:04 +0200

    Pauze menu, hoofd menu, menu scene, credits scene, turorial scene en alle gerelateerde scripts toegevoegd. Speler kan tussen de scenes door navigeren via de buttons en als de speler in een level sit kan de speler het level resetten, terug gaan naar het hoofd menu of terug gaan naar de game.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(Application.loadedLevel);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+
+    public GameObject Menu;
+    GameObject player;
+
+    public void Start () {
+        Menu.SetActive(false);
+        Time.timeScale = 1;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	public void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            player.GetComponent<PlayerMovement>().playerInputEnabled = false;
+            Menu.SetActive(true);
+        }
+    }
+
+    public void Resume()
+    {
+        Menu.SetActive(false);
+        player.GetComponent<PlayerMovement>().playerInputEnabled = true;
+        Time.timeScale = 1;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(Application.loadedLevel);
+    }
+
+    public void mainMenu()
+    {
+        Application.LoadLevel("Menu");
+        Time.timeScale = 1;
+    }
+
+    public IEnumerator WaitForSeconds()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
